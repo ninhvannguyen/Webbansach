b6d2802972fef2ecf20ece2aafef3189f54d3b3a
ninhnguyen <78299736+Ninhnguyen0277@users.noreply.github.com>
2023-05-28 23:51:41 +0700

    demostring

--- src/com/niit/java23/JDBCDemo.java
+++ src/com/niit/java23/JDBCDemo.java
@@ -8,6 +8,22 @@ public class JDBCDemo {
             Class.forName("org.postgresql.Driver");//load driver
             //ket noi den postgresql server
             Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres?user=postgres&password=0967269587");
+
+
+            //thuc thi cau lenh sql
+            // thuc thi cau lenh insert
+            Statement stm = conn.createStatement();
+            stm.executeUpdate("INSERT INTO users(email,pwd,salary) values ('mn@gmail.com','1234',1000)");
+            //cap nhat du lieu
+            ResultSet rs = stm.executeQuery("SELECT * from users ");
+            while (rs.next()) {
+                String email = rs.getString("email");
+                Double salary = rs.getDouble("salary");
+                System.out.println(email);
+                System.out.println(salary);
+
+            } ;
+            //lay du lieu
         } catch (Exception e) {
             e.printStackTrace();
         }
